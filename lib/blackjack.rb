e50609457263bddab37f536497b081d35c420149
Kyle Cole <kylepcole@gmail.com>
2019-10-28 23:59:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,7 +46,8 @@ def hit?(current_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
